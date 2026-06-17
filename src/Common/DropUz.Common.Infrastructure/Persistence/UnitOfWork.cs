15aff8b213b3b9bd6fda01b893492d059cf01791
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2026-06-17 18:09:43 +0500

    feat: register

--- src/Common/DropUz.Common.Infrastructure/Persistence/UnitOfWork.cs
+++ src/Common/DropUz.Common.Infrastructure/Persistence/UnitOfWork.cs
@@ -3,7 +3,7 @@ using DropUz.Common.Infrastructure.Data;
 
 namespace DropUz.Common.Infrastructure.Persistence;
 
-public sealed class UnitOfWork(MainDbContext context) : IUnitOfWork
+internal class UnitOfWork(MainDbContext context) : IUnitOfWork
 {
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
